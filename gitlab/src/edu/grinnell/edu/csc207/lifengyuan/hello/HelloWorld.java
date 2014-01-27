3247c025d85aaf6337ff51158805a0fe37a88bb0
lifengyuan <lifengyu@grinnell.edu>
2014-01-27 05:29:16 -0600

    README

---
+++ gitlab/src/edu/grinnell/edu/csc207/lifengyuan/hello/HelloWorld.java
@@ -0,0 +1,11 @@
+package edu.grinnell.edu.csc207.lifengyuan.hello;
+
+public class HelloWorld {
+	public static void 
+	    main (String[] args) 
+	  {
+	    System.out.println ("Hi, GitHub!");
+	    System.out.println ("Hi, GitHub!");
+	    System.out.println ("Hi, GitHub!");
+	  } // main(String[])
+}
