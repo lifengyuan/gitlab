b2dd6e7052037c91d46a911b8a87c3dd2008fd3e
lifengyuan <lifengyu@grinnell.edu>
2014-01-27 05:38:42 -0600

    Hello to Hi

--- gitlab/src/edu/grinnell/edu/csc207/lifengyuan/hello/HelloWorld.java
+++ gitlab/src/edu/grinnell/edu/csc207/lifengyuan/hello/HelloWorld.java
@@ -4,8 +4,8 @@ public class HelloWorld {
 	public static void 
 	    main (String[] args) 
 	  {
-	    System.out.println ("Hello, GitHub!!");
-	    System.out.println ("Hello, GitHub!!");
-	    System.out.println ("Hello, GitHub!!");
+	    System.out.println ("Hi, GitHub!!");
+	    System.out.println ("Hi, GitHub!!");
+	    System.out.println ("Hi, GitHub!!");
 	  } // main(String[])
 }
